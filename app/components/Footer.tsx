10a8a4a3cc84700a8da2a5b65ed8eff949287394
y2vv <53437471+y2vv@users.noreply.github.com>
2022-11-11 22:54:13 +0900

    フッター修正

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,15 @@
+import { Fredoka_One } from "@next/font/google";
 import Link from "next/link";
+
+const fredokaOneFont = Fredoka_One({ weight: '400', })
 
 export default function Footer() {
   return (
-    <footer className="w-full h-12 border-t-2 border-gray-100 border-opacity-30 p-10 text-gray-200">
-      <div className="flex">
+    <footer className="w-full border-t-2 border-gray-100 border-opacity-30 py-5 px-10 text-gray-200">
+      <div>
+        <Link href={'/'} className={`${fredokaOneFont.className} text-lg`}>Blog title</Link>
+      </div>
+      <div className="flex flex-col gap-1">
         <Link href={"/term"} className='flex-auto'>term</Link>
         <Link href={"/example"} className='flex-auto'>example</Link>
       </div>
