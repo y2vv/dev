f652e03361c6ba31601f1c2607f0b5c506ebeafa
y2vv <53437471+y2vv@users.noreply.github.com>
2022-11-11 22:54:40 +0900

    カード修正

--- app/components/ContentCard.tsx
+++ app/components/ContentCard.tsx
@@ -1,7 +1,7 @@
 export default function ContentCard() {
   return (
-    <div className="w-80 h-36 border-2 border-violet-400 border-opacity-30 p-10 rounded-2xl bg-violet-700 shadow-xl shadow-fuchsia-500/50 backdrop-opacity-60">
-      <div className="divide-y-2 p-2">
+    <div className="w-80 h-36 p-10 rounded-2xl backdrop-blur-xl backdrop-saturate-100 bg-white bg-opacity-5 shadow-lg border-2 border-opacity-20 border-fuchsia-300">
+      <div className="divide-y-2 p-2 divide-opacity-30 divide-white">
         <div className="text-lg text-gray-200 font-bold">title</div>
         <div className=" text-gray-200">content</div>
       </div>
