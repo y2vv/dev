3a5ae37fe48621e82812a1870b1bcd8460f536d6
y2vv <53437471+y2vv@users.noreply.github.com>
2022-11-13 01:40:28 +0900

    サイズ調整

--- app/components/ContentCard.tsx
+++ app/components/ContentCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ContentCard({ href, title }: { href: string, title: string }) {
   return (
     <Link href={href} >
-      <div className=" h-28 w-36 p-3 rounded-2xl backdrop-blur-xl backdrop-saturate-100 bg-black bg-opacity-5 shadow-lg border-2 border-opacity-20 border-gray-300">
+      <div className="h-28 w-28 md:w-36 p-3 rounded-2xl backdrop-blur-xl backdrop-saturate-100 bg-black bg-opacity-5 shadow-lg border-2 border-opacity-20 border-gray-300">
         <div className="divide-y-2 p-2 divide-opacity-30 divide-white">
           <div className="text-md lg:text-lg text-gray-200 font-bold">{title}</div>
         </div>
